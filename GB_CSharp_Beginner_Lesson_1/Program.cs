4993e759921a7d099a043b499fc3570714f5480c
FiRe_MonQ <fire.monq@fmail.com>
2018-11-28 00:37:41 +0300

    Add Lesson1

--- GB_CSharp_Beginner_Lesson_1/Program.cs
+++ GB_CSharp_Beginner_Lesson_1/Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using YUMSHAN;
 
 namespace GB_CSharp_Beginner_Lesson_1
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
+            MyLib.ConsSetup();
+
+            int a, b;
+            double sum, div, avr;
+
+            Console.Write("Input A:");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input B:");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            sum = a + b;
+            Console.WriteLine("SUMM "+ a + "+" + b +" = " + sum);
+
+            div = a / b;
+            Console.WriteLine($"DIV {a} + {b} = {div}");
+                  
+            avr = (double)(a + b)/2.0;
+            Console.WriteLine("AVR ({0} + {1}) / 2 = {2:F3}", a, b, avr);
+
+            MyLib.Pause();
         }
     }
 }
