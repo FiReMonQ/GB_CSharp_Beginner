425516d359c78a4c73f70cd4e1b8accee3f9ec6d
FiRe_MonQ <fire.monq@fmail.com>
2018-11-27 21:57:11 +0300

    Start

---
+++ GB_CSharp_Beginner_Lesson_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_CSharp_Beginner_Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
